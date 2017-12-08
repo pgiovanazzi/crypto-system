178b03dabf98623193beb9378c48a6ef49ac1fdc
paulogo <paulo.go@protonmail.com>
2017-12-07 21:18:33 -0300

    Improve abstraction

--- MatrixEncrypt.c
+++ MatrixEncrypt.c
@@ -70,7 +70,7 @@ void matrix_decode(ValueTable *vt) {
 int main(int argc, char *argv[]) {
   char msg[100];
   int ml = 0;
-	// Formato de entrada
+  // Formato de entrada
   char format[] = "%[^\n]%*c";
   printf("Ingrese el mensaje a codificar:\n");
   scanf(format, msg);
