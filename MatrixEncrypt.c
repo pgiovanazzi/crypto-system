d312f411dceff374e52dd8fc02d2af834f8f89f5
paulogo <paulo.go@protonmail.com>
2017-12-07 21:44:25 -0300

    Improve abstraction

--- MatrixEncrypt.c
+++ MatrixEncrypt.c
@@ -9,24 +9,24 @@ typedef struct _ValueTable {
 } ValueTable;
 
 // tabla de caracteres con los valores del indice de un arreglo
-const char TABLE[] =
-    " abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ!?*$#@&^()+-<>";
+const char TABLE[] = " abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+                     "!?*$#@&^()+-<>0123456789";
 // matriz de codificacion
 const int MATRIX_C[][3] = {{1, 2, 3}, {2, 5, 8}, {3, 6, 10}};
 // matriz de decodificacion
 const int MATRIX_D[][3] = {{2, -2, 1}, {4, 1, -2}, {-3, 0, 1}};
 // Toma un mensaje y su logitud y devuelve su tabla de valores
-ValueTable *matrix_value(char msg[], int msgLen) {
+static ValueTable *matrix_value(char msgIn[], int msgLenIn) {
   ValueTable *vt = malloc(sizeof(ValueTable));
-  vt->sz = msgLen + ((msgLen % 3 != 0) ? (3 - msgLen % 3) : 0);
+  vt->sz = msgLenIn + ((msgLenIn % 3 != 0) ? (3 - msgLenIn % 3) : 0);
   vt->data = malloc(sizeof(int) * vt->sz);
 
   for (size_t i = 0; i < vt->sz; i++)
     vt->data[i] = 0;
 
-  for (size_t i = 0; i < msgLen; i++) {
+  for (size_t i = 0; i < msgLenIn; i++) {
     for (size_t j = 0; j < len(TABLE) - 1; j++) {
-      if (msg[i] == TABLE[j]) {
+      if (msgIn[i] == TABLE[j]) {
         vt->data[i] = j;
         break;
       }
@@ -39,7 +39,7 @@ ValueTable *matrix_value(char msg[], int msgLen) {
 // entero
 // que representara el inicio de cada terna de numeros para multiplicarlos con
 // la matriz.
-void matrix_multiply(ValueTable *vt, const int matrix[][3], int init) {
+static void matrix_multiply(ValueTable *vt, const int matrix[][3], int init) {
   int code;
   int *data = malloc(sizeof(int) * 3);
   for (size_t i = 0; i < 3; i++) {
@@ -55,60 +55,74 @@ void matrix_multiply(ValueTable *vt, const int matrix[][3], int init) {
   free(data);
 }
 // Toma un tabla de valores y la codifica
-void matrix_encode(ValueTable *vt) {
-  for (size_t i = 0; i < vt->sz; i += 3) {
-    matrix_multiply(vt, MATRIX_C, i);
+static void matrix_encode(ValueTable *vtIn) {
+  for (size_t i = 0; i < vtIn->sz; i += 3) {
+    matrix_multiply(vtIn, MATRIX_C, i);
   }
 }
 // Toma una tabla de valores codificada y la decodifica
-void matrix_decode(ValueTable *vt) {
-  for (size_t i = 0; i < vt->sz; i += 3) {
-    matrix_multiply(vt, MATRIX_D, i);
+static void matrix_decode(ValueTable *vtIn) {
+  for (size_t i = 0; i < vtIn->sz; i += 3) {
+    matrix_multiply(vtIn, MATRIX_D, i);
   }
 }
 
-int main(int argc, char *argv[]) {
-  char msg[100];
-  int ml = 0;
+static void matrix_input(char msgIn[]) {
   // Formato de entrada
   char format[] = "%[^\n]%*c";
   printf("Ingrese el mensaje a codificar:\n");
-  scanf(format, msg);
-  // Ajustamos el tamaño
-  for (; msg[ml]; ml++)
-    ;
+  scanf(format, msgIn);
+}
 
-  ValueTable *vt = matrix_value(msg, ml);
-
-  printf("Tabla sin codificar: ");
-  for (size_t i = 0; i < vt->sz; i++) {
-    printf("%d", vt->data[i]);
+static void show_output_interface(const char *format, ValueTable *vtIn) {
+  printf("%-21s", format);
+  for (size_t i = 0; i < vtIn->sz; i++) {
+    printf("%d", vtIn->data[i]);
   }
   puts("");
+}
+
+static void show_output_decode(ValueTable *vtIn) {
+  printf("Su mensaje es: ");
+  for (size_t i = 0; i < vtIn->sz; i++) {
+    printf("%c", TABLE[vtIn->data[i]]);
+  }
+  puts("");
+}
+
+static void set_length_msg(char *msgIn, int *msgLenIn) {
+  // Ajustamos el tamaño
+  for (; msgIn[*msgLenIn]; (*msgLenIn)++)
+    ;
+}
+
+static void matrix_destroy(ValueTable *vtIn) {
+  free(vtIn->data);
+  free(vtIn);
+}
+
+int main(int argc, char *argv[]) {
+  char msgIn[100];
+  int msgLenIn = 0;
+
+  matrix_input(msg);
+  set_length_msg(msg, &msgLen);
+
+  ValueTable *vt = matrix_value(msg, msgLen);
+
+  show_output_interface("Tabla sin codificar:", vt);
 
   matrix_encode(vt);
 
-  printf("Tabla codificada: ");
-  for (size_t i = 0; i < vt->sz; i++) {
-    printf("%d", vt->data[i]);
-  }
-  puts("");
+  show_output_interface("Tabla codificada:", vt);
 
   matrix_decode(vt);
 
-  printf("Tabla decodificada: ");
-  for (size_t i = 0; i < vt->sz; i++) {
-    printf("%d", vt->data[i]);
-  }
-  puts("");
+  show_output_interface("Tabla decodificada:", vt);
 
-  printf("Su mensaje es: ");
-  for (size_t i = 0; i < vt->sz; i++) {
-    printf("%c", TABLE[vt->data[i]]);
-  }
-  puts("");
-  free(vt->data);
-  free(vt);
+  show_output_decode(vt);
+
+  matrix_destroy(vt);
 
   return 0;
 }
