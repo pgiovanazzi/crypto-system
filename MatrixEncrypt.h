4d8034e4f35f59b079bfe4bcacae9d9b34262d85
Paulo <paulo.go@pm.me>
2018-05-14 04:00:02 -0300

    Modificacion de los punteros

--- MatrixEncrypt.h
+++ MatrixEncrypt.h
@@ -5,7 +5,7 @@
 
 typedef struct _ValueTable {
   int sz;
-  int *data;
+  int* data;
 } ValueTable;
 
 // tabla de caracteres con los valores del indice de un arreglo.
@@ -18,18 +18,18 @@ static const int MATRIX_C[][3] = {{1, 2, 3}, {2, 5, 8}, {3, 6, 10}};
 static const int MATRIX_D[][3] = {{2, -2, 1}, {4, 1, -2}, {-3, 0, 1}};
 
 // Toma un mensaje y su logitud y devuelve su tabla de valores.
-ValueTable *matrix_value(char msgIn[], int msgLenIn);
+ValueTable* matrix_value(char msgIn[], int msgLenIn);
 
 // Toma un tabla de valores y la codifica.
-void matrix_encode(ValueTable *vtIn);
+void matrix_encode(ValueTable* vtIn);
 
 // Toma una tabla de valores codificada y la decodifica.
-void matrix_decode(ValueTable *vtIn);
+void matrix_decode(ValueTable* vtIn);
 
 // Calula la longitud del mensaje y lo guarda en msgLenIn.
-void set_length_msg(char *msgIn, int *msgLenIn);
+void set_length_msg(char* msgIn, int* msgLenIn);
 
 // Destruye la tabla de valores.
-void matrix_destroy(ValueTable *vtIn);
+void matrix_destroy(ValueTable* vtIn);
 
 #endif /* __MATRIXENCRYPT_H__ */
